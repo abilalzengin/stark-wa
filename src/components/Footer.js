c40e80a6d13ae30074d30614b5cfba37749f1904
emreg <emregllce@gmail.com>
2024-02-15 12:32:06 +0300

    contact data update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,25 +9,14 @@ const Footer = () => {
     <div className="footer-container">
         <h2>Bize Ulaşın</h2>
       <div className="footer">
-        <div className="double-contact">
           <a href="https://wa.me/905356433720">
             <WhatsAppIcon />
-            <span>Emre Öztürk</span>
           </a>
-          <a href="https://wa.me/905421876737">
-            <WhatsAppIcon /> <span>Gökhan Erdoğan</span>
-          </a>
-        </div>
-        <div className="double-contact">
+          
           <a href="tel:+905356433720">
             <PhoneIcon />
-            <span>Emre Öztürk</span>
           </a>
-          <a href="tel:+905421876737">
-            <PhoneIcon />
-            <span>Gökhan Erdoğan</span>
-          </a>
-        </div>
+         
         <a href="mailto: emre.ozturk37@hotmail.com">
           <MailIcon />
         </a>
